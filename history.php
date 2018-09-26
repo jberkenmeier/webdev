7bc8cb3af1471774f3f726d1f627d1d5bb01e0f3
Josh Berkenmeier <joshuaberkenmeier@u.boisestate.edu>
2018-09-26 11:06:56 -0600

    Fixed linking issue

--- history.php
+++ history.php
@@ -17,7 +17,6 @@
     <h4> MY HISTORY </h4>
 </div>
 
-
    <iframe src="https://calendar.google.com/calendar/embed?height=300&amp;wkst=1&amp;bgcolor=%23333399&amp;src=u.boisestate.edu_3b9se4jfs262g52r696dh5vu1k%40group.calendar.google.com&amp;color=%230F4B38&amp;ctz=America%2FDenver" style="border-width:0" width="100%" height="50%" frameborder="0" scrolling="no"></iframe>
 
 <p> NOTE: Calendar does not actually work yet....</p>
--- home.php
+++ home.php
@@ -15,15 +15,15 @@
             
 <div class="columns">
   <div id="exercises">
-    <a href="/exercises">My Exercises</a>
+    <a href="/exercises.php">My Exercises</a>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
   </div>
   <div id="history">
-    <a href="/history">My History</a>
+    <a href="/history.php">My History</a>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
   </div>
   <div id="prs">
-    <a href="/prs">My PR's</a>
+    <a href="/prs.php">My PR's</a>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
   </div>
   <div class="clear"></div>
